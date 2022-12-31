b2743fc6dbe38b371ff174f3bb99e113a13a6d82
haiderali780 <muhammadhaiderali782002@gmail.com>
2022-12-31 15:55:06 -0800

    Adding custom content provider component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React, { useState, useEffect, useReducer, useContext } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
+import AuthContext from "../store/auth-context";
 
 const emailReducer = (state, action) => {
   if (action.type === "USER_INPUT") {
@@ -27,7 +28,7 @@ const passwordReducer = (state, action) => {
 const Login = (props) => {
   // const [enteredEmail, setEnteredEmail] = useState('');
   // const [emailIsValid, setEmailIsValid] = useState();
-  // const [enteredPassword, setEnteredPassword] = useState("");
+  // const [enteredPassword, setEnteredPassword] = useState('');
   // const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
@@ -35,43 +36,47 @@ const Login = (props) => {
     value: "",
     isValid: null,
   });
-  const [passwordState, dispatchpassword] = useReducer(passwordReducer, {
+  const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
     value: "",
     isValid: null,
   });
 
-  const { isValid: emailValid } = emailState;
-  const { isValid: passwordValid } = passwordState;
+  const authCtx = useContext(AuthContext);
 
-  // useEffect(() => {
-  //   console.log("EFFECT RUNNING");
+  useEffect(() => {
+    console.log("EFFECT RUNNING");
 
-  //   return () => {
-  //     console.log("EFFECT CLEANUP");
-  //   };
-  // }, []);
+    return () => {
+      console.log("EFFECT CLEANUP");
+    };
+  }, []);
+
+  const { isValid: emailIsValid } = emailState;
+  const { isValid: passwordIsValid } = passwordState;
 
   useEffect(() => {
     const identifier = setTimeout(() => {
       console.log("Checking form validity!");
-      setFormIsValid(emailValid && passwordValid);
+      setFormIsValid(emailIsValid && passwordIsValid);
     }, 500);
+
     return () => {
       console.log("CLEANUP");
       clearTimeout(identifier);
     };
-  }, [emailValid, passwordValid]);
+  }, [emailIsValid, passwordIsValid]);
 
   const emailChangeHandler = (event) => {
     dispatchEmail({ type: "USER_INPUT", val: event.target.value });
 
     // setFormIsValid(
-    //   event.target.value.includes("@") && passwordState.isValid
+    //   event.target.value.includes('@') && passwordState.isValid
     // );
   };
 
   const passwordChangeHandler = (event) => {
-    dispatchpassword({ type: "USER_INPUT", val: event.target.value });
+    dispatchPassword({ type: "USER_INPUT", val: event.target.value });
+
     // setFormIsValid(emailState.isValid && event.target.value.trim().length > 6);
   };
 
@@ -80,12 +85,12 @@ const Login = (props) => {
   };
 
   const validatePasswordHandler = () => {
-    dispatchpassword({ type: "INPUT_BLUR" });
+    dispatchPassword({ type: "INPUT_BLUR" });
   };
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authCtx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { AuthContextProvider } from "./components/store/auth-context";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <AuthContextProvider>
+    <App />
+  </AuthContextProvider>
+)

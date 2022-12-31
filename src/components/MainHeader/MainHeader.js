dd99f1cd2d917c66a955ea29e492230bd331c6e2
haiderali780 <muhammadhaiderali782002@gmail.com>
2022-12-31 14:05:58 -0800

    replace fuctionality of logout with the technique of context API instead of props

--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -3,13 +3,18 @@ import React from "react";
 import Navigation from "./Navigation";
 import classes from "./MainHeader.module.css";
 
+
 const MainHeader = (props) => {
   return (
     <header className={classes["main-header"]}>
       <h1>A Typical Page</h1>
-      <Navigation onLogout={props.onLogout} />
+      <Navigation  />
     </header>
   );
 };
+
+
+
+
 
 export default MainHeader;

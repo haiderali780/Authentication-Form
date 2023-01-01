cfb8a598175bdf0d14319388937efcaaaed980c7
haiderali780 <muhammadhaiderali782002@gmail.com>
2023-01-01 01:04:39 -0800

    forwardRef in Input component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,10 @@
-import React, { useState, useEffect, useReducer, useContext } from "react";
+import React, {
+  useState,
+  useEffect,
+  useReducer,
+  useContext,
+  useRef,
+} from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
@@ -43,6 +49,9 @@ const Login = (props) => {
   });
 
   const authCtx = useContext(AuthContext);
+
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
 
   useEffect(() => {
     console.log("EFFECT RUNNING");
@@ -91,13 +100,20 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    authCtx.onLogin(emailState.value, passwordState.value);
+    if (formIsValid) {
+      authCtx.login(emailState.value, passwordState.value);
+    } else if (!emailIsValid) {
+      emailInputRef.current.focus();
+    } else {
+      passwordInputRef.current.focus();
+    }
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
         <Input
+          ref={emailInputRef}
           id="email"
           label="E-Mail"
           type="email"
@@ -106,6 +122,7 @@ const Login = (props) => {
           onBlur={validateEmailHandler}
         ></Input>
         <Input
+          ref={passwordInputRef}
           id="password"
           label="Password"
           type="password"
@@ -113,9 +130,9 @@ const Login = (props) => {
           onChange={passwordChangeHandler}
           onBlur={validatePasswordHandler}
         ></Input>
-       
+
         <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+          <Button type="submit" className={classes.btn}>
             Login
           </Button>
         </div>
--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React,{ useRef,useImperativeHandle} from "react";
 import classes from "./Input.module.css";
 
-const Input = (props) => {
+
+const Input =React.forwardRef( (props,ref) => {
+const inputRef=useRef();
+const activate=()=>{
+    inputRef.current.focus();
+}
+useImperativeHandle(ref,()=>{
+    return{  focus:activate};
+    }
+     )
+
   return (
     <div
       className={`${classes.control} ${
@@ -10,6 +20,7 @@ const Input = (props) => {
     >
       <label htmlFor={props.id}>{props.label}</label>
       <input
+      ref={inputRef}
         type={props.type}
         id={props.id}
         value={props.value}
@@ -18,6 +29,5 @@ const Input = (props) => {
       />
     </div>
   );
-};
-
+});
 export default Input;

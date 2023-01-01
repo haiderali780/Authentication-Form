5e641645fc7990b44a820da8f3ae5cd6b77da5b4
haiderali780 <muhammadhaiderali782002@gmail.com>
2022-12-31 17:34:36 -0800

    fixing error of displaying 2 logout buttons

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,17 +1,12 @@
-import React, { useContext } from "react";
-import AuthContext from "../store/auth-context";
-import Button from "../UI/Button/Button";
+import React from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Home.module.css";
 
 const Home = (props) => {
-  const authCtx = useContext(AuthContext);
-
   return (
     <Card className={classes.home}>
       <h1>Welcome back!</h1>
-      <Button onClick={authCtx.onLogout}>Logout</Button>
     </Card>
   );
 };
